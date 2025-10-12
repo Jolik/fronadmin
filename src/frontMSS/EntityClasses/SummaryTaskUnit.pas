004d6b1b437e410876a6a309ccbb2fd92226de5c
Dzhalil Akhtyamov <ajolik@gmail.com>
2025-10-12 14:30:55 +0300

    Refactor summary tasks to use shared task abstractions

--- src/frontMSS/EntityClasses/SummaryTaskUnit.pas
+++ src/frontMSS/EntityClasses/SummaryTaskUnit.pas
@@ -3,77 +3,32 @@ unit SummaryTaskUnit;
 interface
 
 uses
-  System.Classes, System.SysUtils, System.JSON,
-  FuncUnit,
+  System.Classes, System.JSON, System.Generics.Collections,
+  LoggingUnit,
   EntityUnit, TaskUnit;
 
 type
-  /// <summary>
-  ///   Summary task entity representation.
-  /// </summary>
+  ///    ( summary)
   TSummaryTask = class(TTask)
-  private
-  protected
-    ///  метод возвращает конкретный тип объекта Settings
-    ///  потомки должны переопределить его, потому что он у всех разный
-    class function SettingsClassType: TSettingsClass; override;
-
-  public
-    // эти требуют существующего правильного экземпл€ра объекта. на ошибки - эксешан
-    procedure Parse(src: TJSONObject; const APropertyNames: TArray<string> = nil); overload; override;
-    procedure Serialize(dst: TJSONObject; const APropertyNames: TArray<string> = nil); overload; override;
 
   end;
 
-  /// <summary>
-  ///   Class Settings for TSummaryTask
-  /// </summary>
+type
+  ///    SummaryTask
   TSummaryTaskSettings = class(TSettings)
-  public
-    // эти требуют существующего правильного экземпл€ра объекта. на ошибки - эксешан
-    ///  в массиве const APropertyNames передаютс€ пол€, которые необходимо использовать
-    procedure Parse(src: TJSONObject; const APropertyNames: TArray<string> = nil); override;
-    procedure Serialize(dst: TJSONObject; const APropertyNames: TArray<string> = nil); overload; override;
+
+  end;
+
+type
+  ///
+  TSummaryTaskList = class(TEntityList)
 
   end;
 
 implementation
 
-{ TSummaryTask }
-
-procedure TSummaryTask.Parse(src: TJSONObject;
-  const APropertyNames: TArray<string>);
-begin
-  inherited;
-
-end;
-
-procedure TSummaryTask.Serialize(dst: TJSONObject;
-  const APropertyNames: TArray<string>);
-begin
-  inherited;
-
-end;
-
-class function TSummaryTask.SettingsClassType: TSettingsClass;
-begin
-  Result := TSummaryTaskSettings;
-end;
-
-{ TSummaryTaskSettings }
-
-procedure TSummaryTaskSettings.Parse(src: TJSONObject;
-  const APropertyNames: TArray<string>);
-begin
-  inherited;
-
-end;
-
-procedure TSummaryTaskSettings.Serialize(dst: TJSONObject;
-  const APropertyNames: TArray<string>);
-begin
-  inherited;
-
-end;
+uses
+  System.SysUtils,
+  FuncUnit;
 
 end.
