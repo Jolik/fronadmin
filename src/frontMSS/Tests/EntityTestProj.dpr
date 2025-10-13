3a510830e48f71699df90427179a8c7c8124aa68
Dzhalil Akhtyamov <ajolik@gmail.com>
2025-10-13 23:13:41 +0300

    Add router source display to entity test

--- src/frontMSS/Tests/EntityTestProj.dpr
+++ src/frontMSS/Tests/EntityTestProj.dpr
@@ -5,16 +5,59 @@ program EntityTestProj;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  System.JSON,
+  RouterSourceUnit,
+  RouterSourceBrokerUnit,
+  EntityUnit;
+
+var
+  RouterSourceBroker: TRouterSourceBroker;
+  RouterSourceList: TEntityList;
+  RouterSourceEntity: TEntity;
+  PageCount: Integer;
 
 begin
   try
+    RouterSourceBroker := TRouterSourceBroker.Create;
+    try
+      RouterSourceList := nil;
+      try
+        RouterSourceList := RouterSourceBroker.List(PageCount);
 
-  ///  делаем запрос
-  ///
-  ///  выводим в консоль ответ
-  ///
-  ///
+        Writeln('---------- Router Sources List ----------');
+
+        if Assigned(RouterSourceList) then
+        begin
+          for RouterSourceEntity in RouterSourceList do
+          begin
+            var Source := RouterSourceEntity as TRouterSource;
+            Writeln(Format('Class: %s | Address: %p', [Source.ClassName, Pointer(Source)]));
+            Writeln('Who: ' + Source.Who);
+            Writeln('SvcId: ' + Source.SvcId);
+            Writeln('Lid: ' + Source.Lid);
+
+            Writeln('As JSON:');
+            var Json := Source.Serialize();
+            try
+              if Json <> nil then
+                Writeln(Json.Format);
+            finally
+              Json.Free;
+            end;
+
+            Writeln('----------');
+          end;
+        end
+        else
+          Writeln('Router source list is empty.');
+
+      finally
+        RouterSourceList.Free;
+      end;
+    finally
+      RouterSourceBroker.Free;
+    end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
