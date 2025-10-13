37d6c26a7687a18ed370870c5451f1e4a540e0a0
Dzhalil Akhtyamov <ajolik@gmail.com>
2025-10-13 23:22:44 +0300

    ППоправил тест за ИИ

--- src/frontMSS/Tests/EntityTestProj.dpr
+++ src/frontMSS/Tests/EntityTestProj.dpr
@@ -7,15 +7,44 @@
 uses
   System.SysUtils,
   System.JSON,
-  RouterSourceUnit,
-  RouterSourceBrokerUnit,
-  EntityUnit;
+  APIConst in '..\APIClasses\APIConst.pas',
+  ChannelsBrokerUnit in '..\APIClasses\ChannelsBrokerUnit.pas',
+  LinksBrokerUnit in '..\APIClasses\LinksBrokerUnit.pas',
+  MainHttpModuleUnit in '..\APIClasses\MainHttpModuleUnit.pas',
+  MonitoringTasksBrokerUnit in '..\APIClasses\MonitoringTasksBrokerUnit.pas',
+  ParentBrokerUnit in '..\APIClasses\ParentBrokerUnit.pas',
+  QueuesBrokerUnit in '..\APIClasses\QueuesBrokerUnit.pas',
+  RouterSourceBrokerUnit in '..\APIClasses\RouterSourceBrokerUnit.pas',
+  StripTasksBrokerUnit in '..\APIClasses\StripTasksBrokerUnit.pas',
+  SummaryTasksBrokerUnit in '..\APIClasses\SummaryTasksBrokerUnit.pas',
+  TasksBrokerUnit in '..\APIClasses\TasksBrokerUnit.pas',
+  LoggingUnit in '..\Logging\LoggingUnit.pas',
+  TextFileLoggerUnit in '..\Logging\TextFileLoggerUnit.pas',
+  ConstsUnit in '..\Common\ConstsUnit.pas',
+  FuncUnit in '..\Common\FuncUnit.pas',
+  ConnectionUnit in '..\EntityClasses\Common\ConnectionUnit.pas',
+  EntityUnit in '..\EntityClasses\Common\EntityUnit.pas',
+  ScheduleUnit in '..\EntityClasses\Common\ScheduleUnit.pas',
+  TaskUnit in '..\EntityClasses\Common\TaskUnit.pas',
+  LinkSettingsUnit in '..\EntityClasses\links\LinkSettingsUnit.pas',
+  LinkUnit in '..\EntityClasses\links\LinkUnit.pas',
+  MonitoringTaskUnit in '..\EntityClasses\monitoring\MonitoringTaskUnit.pas',
+  ChannelUnit in '..\EntityClasses\router\ChannelUnit.pas',
+  QueueUnit in '..\EntityClasses\router\QueueUnit.pas',
+  RouterSourceUnit in '..\EntityClasses\router\RouterSourceUnit.pas',
+  StripTaskUnit in '..\EntityClasses\strips\StripTaskUnit.pas',
+  SummaryTaskUnit in '..\EntityClasses\summary\SummaryTaskUnit.pas';
 
+
+procedure ListRouterSource();
 var
   RouterSourceBroker: TRouterSourceBroker;
   RouterSourceList: TEntityList;
   RouterSourceEntity: TEntity;
   PageCount: Integer;
+
+var
+  Body, Response: string;
 
 begin
   try
@@ -61,82 +90,14 @@ begin
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
-  APIConst in '..\APIClasses\APIConst.pas',
-  ChannelsBrokerUnit in '..\APIClasses\ChannelsBrokerUnit.pas',
-  LinksBrokerUnit in '..\APIClasses\LinksBrokerUnit.pas',
-  MainHttpModuleUnit in '..\APIClasses\MainHttpModuleUnit.pas',
-  MonitoringTasksBrokerUnit in '..\APIClasses\MonitoringTasksBrokerUnit.pas',
-  ParentBrokerUnit in '..\APIClasses\ParentBrokerUnit.pas',
-  QueuesBrokerUnit in '..\APIClasses\QueuesBrokerUnit.pas',
-  RouterSourceBrokerUnit in '..\APIClasses\RouterSourceBrokerUnit.pas',
-  StripTasksBrokerUnit in '..\APIClasses\StripTasksBrokerUnit.pas',
-  SummaryTasksBrokerUnit in '..\APIClasses\SummaryTasksBrokerUnit.pas',
-  TasksBrokerUnit in '..\APIClasses\TasksBrokerUnit.pas',
-  LoggingUnit in '..\Logging\LoggingUnit.pas',
-  TextFileLoggerUnit in '..\Logging\TextFileLoggerUnit.pas',
-  ConstsUnit in '..\Common\ConstsUnit.pas',
-  FuncUnit in '..\Common\FuncUnit.pas',
-  ConnectionUnit in '..\EntityClasses\Common\ConnectionUnit.pas',
-  EntityUnit in '..\EntityClasses\Common\EntityUnit.pas',
-  ScheduleUnit in '..\EntityClasses\Common\ScheduleUnit.pas',
-  TaskUnit in '..\EntityClasses\Common\TaskUnit.pas',
-  LinkSettingsUnit in '..\EntityClasses\links\LinkSettingsUnit.pas',
-  LinkUnit in '..\EntityClasses\links\LinkUnit.pas',
-  MonitoringTaskUnit in '..\EntityClasses\monitoring\MonitoringTaskUnit.pas',
-  ChannelUnit in '..\EntityClasses\router\ChannelUnit.pas',
-  QueueUnit in '..\EntityClasses\router\QueueUnit.pas',
-  RouterSourceUnit in '..\EntityClasses\router\RouterSourceUnit.pas',
-  StripTaskUnit in '..\EntityClasses\strips\StripTaskUnit.pas',
-  SummaryTaskUnit in '..\EntityClasses\summary\SummaryTaskUnit.pas';
-
-var
-  Body, Response: string;
-
-procedure List(BrokerType: TParentBrokerClass);
-var
-  Broker : TParentBroker;
-  Entity : TEntity;
-  EntityList : TEntityList;
-  Pages : integer;
-
-begin
-  try
-    Broker := BrokerType.Create();
-
-    EntityList := Broker.List(Pages);
-
-    writeln('----------  List  ----------');
-    writeln('List test:');
-
-    for Entity in EntityList do
-    begin
-      writeln(Format('ClassName: %s  |  Àäðåñ: %p', [Entity.ClassName, Pointer(Entity)]));
-      writeln('Id '+ Entity.Id);
-      writeln('Name '+ Entity.Name);
-      writeln('Caption '+ Entity.Caption);
-      writeln('Compid '+ (Entity.Compid));
-      writeln('Depid '+ (Entity.Depid));
-
-      writeln('as json:');
-
-      var json := Entity.Serialize();
-
-      if json <> nil then
-        writeln(json.Format());
-
-      writeln('----------');
-    end;
-
-  finally
-    EntityList.Free;
-    Broker.Free;
   end;
 end;
+
 
 begin
   try
     try
-      List(TRouterSourceBroker);
+      ListRouterSource();
 
       // оставить консоль незакрытой до нажатия Enter
       Writeln('press enter to finish...');
