b202abe14f4d100bda84f27caf8a8f77847b495e
Dzhalil Akhtyamov <ajolik@gmail.com>
2025-10-17 00:19:18 +0300

    Поправил за ИИ

--- src/frontMSS/Tests/EntityTestProj.dpr
+++ src/frontMSS/Tests/EntityTestProj.dpr
@@ -117,98 +117,6 @@ begin
   end;
 end;
 
-procedure ListTaskSources();
-procedure ListStripTaskSources();
-procedure ListSummaryTaskSources();
-var
-  TaskSourceBroker: TTaskSourcesBroker;
-  TaskSourceList: TEntityList;
-  TaskSourceEntity: TEntity;
-  PageCount: Integer;
-begin
-  try
-    TaskSourceBroker := TTaskSourcesBroker.Create;
-    try
-      TaskSourceBroker.AddPath := '/5d159669-0cb0-11ec-a8d8-00ff1185687c';
-
-      TaskSourceList := nil;
-      try
-        TaskSourceList := TaskSourceBroker.List(PageCount);
-
-        Writeln('---------- Task Sources List ----------');
-
-        if Assigned(TaskSourceList) then
-        begin
-          var FirstSource: TTaskSource := nil;
-
-          if TaskSourceList.Count > 0 then
-            FirstSource := TaskSourceList.Items[0] as TTaskSource;
-
-          for TaskSourceEntity in TaskSourceList do
-          begin
-            var Source := TaskSourceEntity as TTaskSource;
-            Writeln(Format('Class: %s | Address: %p', [Source.ClassName, Pointer(Source)]));
-            Writeln('Sid: ' + Source.Sid);
-            Writeln('Name: ' + Source.Name);
-            Writeln('Enabled: ' + BoolToStr(Source.Enabled, True));
-            Writeln('Index: ' + Source.StationIndex);
-
-            Writeln('As JSON:');
-            var Json := Source.Serialize();
-            try
-              if Json <> nil then
-                Writeln(Json.Format);
-            finally
-              Json.Free;
-            end;
-
-            Writeln('----------');
-          end;
-
-          if Assigned(FirstSource) then
-          begin
-            Writeln('---------- Task Source Info ----------');
-            Writeln('Requesting info for: ' + FirstSource.Sid);
-
-            var InfoEntity: TEntity := nil;
-            try
-              InfoEntity := TaskSourceBroker.Info(FirstSource.Sid);
-
-              if Assigned(InfoEntity) then
-              begin
-                var InfoSource := InfoEntity as TTaskSource;
-                Writeln('Info result as JSON:');
-                var InfoJson := InfoSource.Serialize();
-                try
-                  if InfoJson <> nil then
-                    Writeln(InfoJson.Format);
-                finally
-                  InfoJson.Free;
-                end;
-              end
-              else
-                Writeln('Info request returned nil.');
-            finally
-              InfoEntity.Free;
-            end;
-            Writeln('----------');
-          end;
-        end
-        else
-          Writeln('Task sources list is empty.');
-
-      finally
-        TaskSourceList.Free;
-      end;
-    finally
-      TaskSourceBroker.Free;
-    end;
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
-end;
-
 procedure ListStripTaskSources();
 var
   StripTasksBroker: TStripTasksBroker;
@@ -1120,9 +1028,8 @@ begin
     try
 //      ListSourceCreds();
 //      ListRouterSource();
-      ListSummaryTaskTypes();
-      ListTaskSources();
-      ListStripTaskSources();
+//      ListSummaryTaskTypes();
+//      ListStripTaskSources();
       ListSummaryTaskSources();
 //      ListAliases();
 //      ListRules();
